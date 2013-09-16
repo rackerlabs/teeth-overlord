fc496be224ed1ec81624a1606c681b9060633cd3
Russell Haering <russell.haering@rackspace.com>
2013-09-16 11:15:11 -0700

    begin being able to send commands to agents

--- teeth/overlord/agent_endpoint.py
+++ teeth/overlord/agent_endpoint.py
@@ -15,6 +15,7 @@ limitations under the License.
 """
 
 import simplejson as json
+import uuid
 
 from klein import Klein
 from twisted.protocols.basic import LineReceiver
@@ -25,15 +26,19 @@ from twisted.internet import reactor, threads
 from teeth.overlord import models
 
 
+DEFAULT_PROTOCOL_VERSION = 'v1'
+
+
 class AgentEndpointHandler(LineReceiver):
     encoder = models.ModelEncoder('public')
+    endpoint = None
 
     def __init__(self):
         self.handlers = {}
         self.handlers['v1'] = {
             'handshake': self.handle_handshake,
         }
-        self.connection = models.AgentConnection()
+        self.connection = models.AgentConnection(id=uuid.uuid4())
 
     def lineReceived(self, line):
         line = line.strip()
@@ -56,14 +61,27 @@ class AgentEndpointHandler(LineReceiver):
                 'result': result,
             }))
 
+    def send_command(self, command):
+        message_id = str(uuid.uuid4())
+        self.sendLine(self.encoder.encode({
+            'id': message_id,
+            'version': DEFAULT_PROTOCOL_VERSION,
+            'method': command['method'],
+            'args': command.get('args', []),
+            'kwargs': command.get('kwargs', {}),
+        }))
+        return 'sent'
+
     def connectionLost(self, reason):
+        self.endpoint.unregister_agent_protocol[self.connection.id]
         threads.deferToThread(self.connection.delete)
 
     def handle_handshake(self, primary_mac_address, agent_version):
         self.connection.primary_mac_address = primary_mac_address
         self.connection.agent_version = agent_version
-        self.connection.endpoint_rpc_host = 'localhost'
-        self.connection.endpoint_rpc_port = 8082
+        self.connection.endpoint_rpc_host = self.config.AGENT_ENDPOINT_RPC_HOST
+        self.connection.endpoint_rpc_port = self.config.AGENT_ENDPOINT_RPC_PORT
+        self.endpoint.register_agent_protocol(self.connection.id, self)
 
         def _saved(result):
             return self.connection
@@ -74,6 +92,14 @@ class AgentEndpointHandler(LineReceiver):
 class AgentEndpointHandlerFactory(ServerFactory):
     protocol = AgentEndpointHandler
 
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+
+    def buildProtocol(self, address):
+        protocol = ServerFactory.buildProtocol(self, address)
+        protocol.endpoint = self.endpoint
+        return protocol
+
 
 class AgentEndpoint(object):
     app = Klein()
@@ -81,17 +107,29 @@ class AgentEndpoint(object):
     def __init__(self, config):
         self.encoder = models.ModelEncoder('public', indent=4)
         self.config = config
+        self.agent_protocols = {}
+
+    def register_agent_protocol(self, connection_id, protocol):
+        print "Registered agent connection {}".format(str(connection_id))
+        self.agent_protocols[str(connection_id)] = protocol
+
+    def unregister_agent_protocol(self, connection_id):
+        del self.agent_protocols[str(connection_id)]
 
     @app.route('/v1.0/agent_connections/<string:connection_id>/command')
     def send_agent_command(self, request, connection_id):
-        return self.encoder.encode({'foo': 'bar'})
+        if not connection_id in self.agent_protocols:
+            request.setResponseCode(404)
+            return
+
+        return self.agent_protocols[connection_id].send_command(json.loads(request.content.read()))
 
     def run(self):
         agent_port = self.config.AGENT_ENDPOINT_AGENT_PORT
         agent_host = self.config.AGENT_ENDPOINT_AGENT_HOST
         rpc_port = self.config.AGENT_ENDPOINT_RPC_PORT
         rpc_host = self.config.AGENT_ENDPOINT_RPC_HOST
-        reactor.listenTCP(agent_port, AgentEndpointHandlerFactory(), interface=agent_host)
+        reactor.listenTCP(agent_port, AgentEndpointHandlerFactory(self), interface=agent_host)
         # Klein calls reactor.run() - we'll need to refactor to not use app.run() anymore
         self.app.run(rpc_host, rpc_port)
         # reactor.run()
