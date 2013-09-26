1d77aba9b08a390d7a912e96a400a606f578cbb2
Russell Haering <russell.haering@rackspace.com>
2013-09-26 09:11:24 -0700

    add a trailing newline to response bodies

--- teeth_overlord/rest.py
+++ teeth_overlord/rest.py
@@ -55,7 +55,7 @@ class RESTServer(service.TeethService):
     def return_ok(self, request, result):
         request.setResponseCode(200)
         request.setHeader('Content-Type', 'application/json')
-        return self.encoder.encode(result)
+        return self.encoder.encode(result) + '\n'
 
     def return_created(self, request, path):
         request.setResponseCode(201)
