a2b3cfceff74f99448d6e308abac075b28836c4e
Jim Rollenhagen <jim.rollenhagen@rackspace.com>
2013-12-27 13:39:52 -0800

    finish up CreateInstance tests

--- teeth_overlord/tests/unit/jobs/instances.py
+++ teeth_overlord/tests/unit/jobs/instances.py
@@ -26,7 +26,6 @@ class CreateInstanceTestCase(tests.TeethAPITestCase):
 
         self.instance_objects_mock = self.add_mock(models.Instance)
         self.chassis_objects_mock = self.add_mock(models.Chassis)
-        self.job_request_objects_mock = self.add_mock(models.JobRequest)
 
         self.instance = models.Instance(id='test_instance',
                                         state=models.InstanceState.BUILD,
@@ -43,6 +42,9 @@ class CreateInstanceTestCase(tests.TeethAPITestCase):
                                              job_type='instances.create',
                                              params=request_params)
 
+        self.instance_objects_mock.return_value = [self.instance]
+        self.chassis_objects_mock.return_value = [self.chassis]
+
         self.executor = jobs_tests_base.MockJobExecutor()
         self.message = {
             'ttl': 1200,
@@ -54,27 +56,48 @@ class CreateInstanceTestCase(tests.TeethAPITestCase):
 
     def _did_prepare_and_run_image(self):
         client = self.executor.agent_client
-        client.get_agent_connection.assert_called_once_with(self.chassis)
+        client.get_agent_connection.assert_called_once()
         client.prepare_image.assert_called_once()
         client.run_image.assert_called_once()
 
     def test_prepare_and_run_image(self):
         image_info = self.executor.image_provider.get_image_info('image_id')
         self.job.prepare_and_run_image(self.instance, self.chassis, image_info)
+        client = self.executor.agent_client
+        client.get_agent_connection.assert_called_once_with(self.chassis)
         self._did_prepare_and_run_image()
 
-    def _is_marked_active(self):
+    def test_prepare_and_run_image_with_connection(self):
+        image_info = self.executor.image_provider.get_image_info('image_id')
+        client = self.executor.agent_client
+        connection = client.get_agent_connection(self.chassis)
+
+        self.job.prepare_and_run_image(self.instance,
+                                       self.chassis,
+                                       image_info,
+                                       connection=connection)
+        client.prepare_image.assert_called_once_with(connection, image_info)
+        client.run_image.assert_called_once_with(connection, image_info)
+
+    def _instance_is_marked_active(self):
+        instance_batch_mock = self.get_mock(models.Instance, 'batch')
+        self.assertEqual(instance_batch_mock().save.call_count, 1)
         self.assertEqual(self.instance.state, models.InstanceState.ACTIVE)
+
+    def _chassis_is_marked_active(self):
+        chassis_batch_mock = self.get_mock(models.Chassis, 'batch')
+        self.assertEqual(chassis_batch_mock().save.call_count, 1)
         self.assertEqual(self.chassis.state, models.ChassisState.ACTIVE)
         self.assertEqual(self.chassis.instance_id, self.instance.id)
 
     def test_mark_active(self):
         self.job.mark_active(self.instance, self.chassis)
-        self._is_marked_active()
+        self._instance_is_marked_active()
+        self._chassis_is_marked_active()
 
     def test_instance_create_job(self):
-        self.job._execute()
         scheduler = self.executor.scheduler
+        self.job._execute()
         scheduler.reserve_chassis.assert_called_once_with(self.instance)
         self._did_prepare_and_run_image()
-        self._is_marked_active()
+        self._instance_is_marked_active()
