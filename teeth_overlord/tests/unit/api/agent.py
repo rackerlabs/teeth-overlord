743b5b50cfea6e046352a9e9f4a35e77fd19e9bc
Jim Rollenhagen <jim.rollenhagen@rackspace.com>
2014-01-06 16:17:30 -0800

    add agent API service tests

---
+++ teeth_overlord/tests/unit/api/agent.py
@@ -0,0 +1,67 @@
+"""
+Copyright 2013 Rackspace, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from teeth_overlord.api import agent as agent_api
+from teeth_overlord import models
+from teeth_overlord import tests
+
+
+class TestAgentAPI(tests.TeethAPITestCase):
+
+    def setUp(self):
+        super(TestAgentAPI, self).setUp()
+        self.api = agent_api.TeethAgentAPIServer(self.config)
+
+        self.url = '/v1.0/agents/00:00:00:00:00:00'
+        self.agent_model_mock = self.add_mock(models.Agent)
+
+    def test_create_agent(self):
+        data = {
+            'primary_mac_address': '00:00:00:00:00:00',
+            'version': '0.1',
+            'url': 'http://10.0.1.1:51200',
+            'mode': models.AgentState.STANDBY,
+        }
+        return_value = [models.Agent(**data)]
+        self.agent_model_mock.return_value = return_value
+        self.agent_model_mock.side_effect = models.Agent.DoesNotExist
+
+        self._update_request(data)
+
+    def _update_request(self, data):
+        response = self.make_request('PUT', self.url, data=data)
+
+        save_mock = self.get_mock(models.Agent, 'save')
+        self.assertEqual(save_mock.call_count, 1)
+        agent = save_mock.call_args[0][0]
+
+        self.assertEqual(agent.primary_mac_address, '00:00:00:00:00:00')
+        self.assertEqual(agent.version, '0.1')
+        self.assertEqual(agent.url, 'http://10.0.1.1:51200')
+        self.assertEqual(agent.mode, models.AgentState.STANDBY)
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_update_agent(self):
+        data = {
+            'primary_mac_address': '00:00:00:00:00:00',
+            'version': '0.1',
+            'url': 'http://10.0.1.1:51200',
+            'mode': models.AgentState.STANDBY,
+        }
+        return_value = [models.Agent(**data)]
+        self.agent_model_mock.return_value = return_value
+        self._update_request(data)
