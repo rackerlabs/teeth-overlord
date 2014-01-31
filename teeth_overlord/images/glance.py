306065f2049a75ba9cf6071de436aded32cc889a
Josh Gachnang <josh.gachnang@rackspace.com>
2014-01-30 17:11:00 -0800

    Sending Keystone auth with Glance images
    Downloading Glance images requires a Keystone auth token, so this token is
    sent along with the image info so agents can download the images.

--- teeth_overlord/images/glance.py
+++ teeth_overlord/images/glance.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import collections
 
 import glanceclient
 from glanceclient import exc as glance_exceptions
@@ -22,6 +23,23 @@ from keystoneclient.apiclient import exceptions as keystone_exceptions
 from keystoneclient.v2_0 import client as keystone_client
 
 from teeth_overlord.images import base
+
+
+class GlanceImageInfo(base.ImageInfo):
+    """Information about an image hosted in Glance."""
+    def __init__(self, **kwargs):
+        self.extra_headers = kwargs.pop('extra_headers', {})
+        super(GlanceImageInfo, self).__init__(**kwargs)
+
+    def serialize(self):
+        """Turn a GlanceImageInfo into a dict."""
+        return collections.OrderedDict([
+            ('id', self.id),
+            ('name', self.name),
+            ('urls', self.urls),
+            ('hashes', self.hashes),
+            ('extra_headers', self.extra_headers)
+        ])
 
 
 class GlanceImageProvider(base.BaseImageProvider):
@@ -40,11 +58,11 @@ class GlanceImageProvider(base.BaseImageProvider):
             raise self.ImageProviderException(
                 'Cannot Initialize Keystone Client: {}'.format(str(e)))
 
-    def _get_glance_client(self):
+    def _get_glance_client(self, auth_token):
         try:
             return glanceclient.Client(self.config.GLANCE_VERSION,
                                        endpoint=self.config.GLANCE_URL,
-                                       token=self._get_auth_token())
+                                       token=auth_token)
         except glance_exceptions.BaseException as e:
             raise self.ImageProviderException(
                 'Cannot Initialize Glance Client: {}'.format(str(e)))
@@ -57,14 +75,19 @@ class GlanceImageProvider(base.BaseImageProvider):
     def _get_hashes(self, image):
         return {'md5': image['checksum']}
 
-    def _make_image_info(self, image):
-        return base.ImageInfo(id=image['id'],
-                              name=image['name'],
-                              urls=self._get_urls(image),
-                              hashes=self._get_hashes(image))
+    def _make_image_info(self, image, auth_token):
+        extra_headers = {
+            'X-Keystone-Token': auth_token
+        }
+        return GlanceImageInfo(id=image['id'],
+                               name=image['name'],
+                               urls=self._get_urls(image),
+                               hashes=self._get_hashes(image),
+                               extra_headers=extra_headers)
 
     def get_image_info(self, image_id):
-        glance = self._get_glance_client()
+        auth_token = self._get_auth_token()
+        glance = self._get_glance_client(auth_token)
 
         try:
             image = glance.images.get(image_id)
@@ -75,10 +98,12 @@ class GlanceImageProvider(base.BaseImageProvider):
             raise self.ImageProviderException(
                 'Cannot Get Image From Glance: {}'.format(str(e)))
 
-        return self._make_image_info(image)
+        image_info = self._make_image_info(image, auth_token)
+        return image_info
 
     def list_images(self):
-        glance = self._get_glance_client()
+        auth_token = self._get_auth_token()
+        glance = self._get_glance_client(auth_token)
 
         try:
             images = [i for i in glance.images.list()]
@@ -86,4 +111,4 @@ class GlanceImageProvider(base.BaseImageProvider):
             raise self.ImageProviderException(
                 'Cannot List Images From Glance: {}'.format(str(e)))
 
-        return [self._make_image_info(i) for i in images]
+        return [self._make_image_info(i, auth_token) for i in images]
